249ff6c49a3698435240d8f4a890f3b793164b31
Al Madireddy <al.madireddy@gmail.com>
2020-06-30 18:04:28 -0500

    Initial Commit

---
+++ The Big List/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  The Big List
+//
+//  Created by Aahlad Madireddy on 6/30/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ The Big List/The_Big_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Big_ListApp.swift
+//  The Big List
+//
+//  Created by Aahlad Madireddy on 6/30/20.
+//
+
+import SwiftUI
+
+@main
+struct The_Big_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
